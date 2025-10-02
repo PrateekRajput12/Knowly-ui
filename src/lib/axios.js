5b388ae227746217ee96b3af6b0fba691ec86e2c
PrateekRajput12 <ficredinn@gmail.com>
2025-10-03 01:13:18 +0530

    done

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === 'https://knowly-server.onrender.com'
+const BASE_URL = import.meta.env.MODE === 'https://knowly-server-1.onrender.com'
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
